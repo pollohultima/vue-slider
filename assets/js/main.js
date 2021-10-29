1d0530ca1e5eb53f012e329f376e1cde9c5e7f3d
pollohultima <matteo.polonio@gmail.com>
2021-10-29 15:05:59 +0200

    updating css on thumbs section

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,37 +3,37 @@ const app = new Vue({
     el: '#root',
 
     data: {
+
+
+
         slides:
             [
                 {
-                    item: 'img / 01.jpg',
+                    item: 'assets/img/01.jpg',
                     title: 'Svezia',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
                 },
                 {
-                    item: 'img / 02.jpg',
+                    item: 'assets/img/02.jpg',
                     title: 'Svizzera',
                     text: 'Lorem ipsum.'
                 },
                 {
-                    item: 'img / 03.jpg',
+                    item: 'assets/img/03.jpg',
                     title: 'Gran Bretagna',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
                 },
                 {
-                    item: 'img / 04.jpg',
+                    item: 'assets/img/04.jpg',
                     title: 'Germania',
                     text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
                 },
                 {
-                    item: 'img / 05.jpg',
+                    item: 'assets/img/05.jpg',
                     title: 'Paradise',
                     text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.'
                 }
             ]
-
-
-
     },
 
     method: {
